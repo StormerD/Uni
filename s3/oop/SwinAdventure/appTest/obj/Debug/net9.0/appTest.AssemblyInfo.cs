0452cbee0313715bca2c8c317c6dbc2cad4a525a
StormerD <drodwell2005@gmail.com>
2025-05-16 16:01:21 +1000

    worked on Security Lab05

--- s3/oop/SwinAdventure/appTest/obj/Debug/net9.0/appTest.AssemblyInfo.cs
+++ s3/oop/SwinAdventure/appTest/obj/Debug/net9.0/appTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73a9b01901c2683d156122814b5ab799d81ecba5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a45945577564f84536381087c66aecb348236d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("appTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
