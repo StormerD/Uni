082d4992b27c6815d2607dad72603e18994b1380
StormerD <drodwell2005@gmail.com>
2025-06-06 19:00:05 +1000

    oop project

--- s3/oop/Shapes/obj/Debug/net9.0/Shapes.AssemblyInfo.cs
+++ s3/oop/Shapes/obj/Debug/net9.0/Shapes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shapes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54b07aba54da2517c8d21a6c39b9d655717272c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7374fe3d20f3b6d21c5bc2a27fb09f3326c29ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shapes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shapes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
